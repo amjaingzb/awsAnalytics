60880ae34b8d6f1df5e55441116139e399c18eb6
Amit Jain <amjain.gzb@gmail.com>
2016-09-27 13:04:54 +0000

    The parameters are now passed from webpage to the server - startYear and endYear

--- T3_WebJob/analytics.js
+++ T3_WebJob/analytics.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
+//var app = require('express').createServer();
+var fs  = require('fs') ;
 
 var myRes1 = function (req, res) {
     res.send('Hello Duniya!');
 };
 var myRes2 = function (req, res) {
-//    res.send('Hello Ankit!');
-      fs = require('fs')
       fs.readFile('data.json', 'utf8', function (err,data) {
           if (err) { return console.log(err); }
 //          console.log(data);
@@ -20,6 +20,31 @@ var myRes2 = function (req, res) {
 };
 app.get('/',myRes1);
 app.get('/ankit',myRes2);
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+/*
+var fProcessPostFromYearForm = function(req, res) {
+  // Prepare output in JSON format
+  response = {
+    startYear:req.body.startYear,
+    endYear:req.body.endYear
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+};
+app.post('/process_yearRange', fProcessPostFromYearForm);
+*/
+
+httpServer = app.listen(3000, function () {
+    console.log('Analytics app listening on port 3000!');
+});
+
+var io  = require('socket.io')(httpServer);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('graphChangeMsg', function(msg){
+    console.log('message: ' + msg);
+  });
 });
